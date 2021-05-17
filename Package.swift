79c97a2bcb33f6813629e5bf6cac703b0316120f
highthem <highthem@icloud.com>
2021-05-17 19:19:36 +0200

    upgrade swift format version

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.41.2"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.2"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.4"),
     ],
